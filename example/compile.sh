d107031b1b40b93e578799c1f9840f5a901839d0
raksa <eng.raksa@gmail.com>
2021-01-28 13:25:54 +0700

    add compile

--- example/compile.sh
+++ example/compile.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-clang test.c -o test \
+name=test
+local_lib=../src/.libs
+
+clang -o $name \
 -I/usr/local/opt/freetype/include/freetype2 \
 -I../src \
 -L../src/.libs -lraqm \
--lfreetype
+-lfreetype \
+$name.c
+
+install_name_tool -change "/usr/local/lib/libraqm.0.dylib" "@executable_path/$local_lib/libraqm.0.dylib" test
